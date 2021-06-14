669440649c26a75fc747c827857048310de27a69
Kelvin Chua <chuakid@gmail.com>
2021-06-14 14:52:54 +0800

    Removed log

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,6 @@
 import { io } from "socket.io-client";
 import { host } from "./variables.js"
 let socket = io(host)
-console.log(host);
 
 socket.on("roomJoinStatus", (status) => {
     console.log("Joined room");
